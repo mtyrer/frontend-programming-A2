51121e7c01263c672d69e740717f293daa5bd299
mtyrer <mtyrer@gmail.com>
2017-07-10 17:33:43 +0200

    nearly there

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,24 @@
 /* This function stretches the side bar so that the all stories button can be positioned */
+function sidebarsize () {
+    if (window.matchMedia('(min-width: 1200px)').matches) {
+        $(".section-row").each(function() {
+            var h = $(this).height();
+            $(".side-sect", $( this ) ).height(h);
+        });
+     } else {
+        /* $(".section-row").each(function() {
+            $(".side-sect", $( this ) ).height(0);
+        }); */
+     }
+}
+
 
 $(document).ready(function () {
-    $(".section-row").each(function() {
-        var h = $(this).height();
-        $(".sidebar", $( this ) ).height(h);
-    });
+     sidebarsize();
+});
+
+$(window).resize(function(){
+    window.location.href = window.location.href;
+    sidebarsize(); 
 });
 
